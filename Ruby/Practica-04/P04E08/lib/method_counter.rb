52837bedb4656c4516a73a0d7051a8e39c89db1c
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-21 13:12:36 -0300

    Ej8

--- Ruby/Practica-04/P04E08/lib/method_counter.rb
+++ Ruby/Practica-04/P04E08/lib/method_counter.rb
@@ -1,7 +1,32 @@
-require 'countable.rb'
+module MethodCounter
+  module Countable
+    def invocations
+      @invocations ||= Hash.new(0)
+    end
 
-class MethodCounter
-  def self.Countable
-    Countable
+    module ClassMethods
+      def count_invocations_of(*args)
+        args.each do |sym|
+          alias_method(:"o_#{sym}", sym)
+
+          define_method sym.to_s do
+            invocations[__method__] += 1
+            send(:"o_#{__method__}")
+          end
+        end
+      end
+    end
+
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
+    def invoked?(sym)
+      invocations[sym] > 0
+    end
+
+    def invoked(sym)
+      invocations[sym]
+    end
   end
 end
